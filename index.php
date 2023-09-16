64962eb3ead760f88105f5099304d144da4b6ba4
Daniel Silva <devdanielsilva@gmail.com>
2023-09-16 16:22:07 -0300

    print

--- index.php
+++ index.php
@@ -1,18 +1,4 @@
 <?php
-
-?>
-
-<!DOCTYPE html>
-<html lang="pt-br">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    <?php
     $txt1 = "Aprendendo PHP";
     $txt2 = "Variáveis com Print";
     $x = 5;
@@ -21,7 +7,4 @@
     print "<h2>" . $txt1 . "</h2>";
     print "Estudando " . $txt2 . "<br>";
     print $x + $y;
-    ?>
-</body>
-
-</html>
+?>
