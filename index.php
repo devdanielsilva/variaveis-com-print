c6f31700f36becfb34519f78b7cc95a5a3ddbc60
Daniel Silva <devdanielsilva@gmail.com>
2023-09-16 16:20:54 -0300

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,16 @@
 </head>
 
 <body>
+    <?php
+    $txt1 = "Aprendendo PHP";
+    $txt2 = "Variáveis com Print";
+    $x = 5;
+    $y = 4;
 
+    print "<h2>" . $txt1 . "</h2>";
+    print "Estudando " . $txt2 . "<br>";
+    print $x + $y;
+    ?>
 </body>
 
 </html>
